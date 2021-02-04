4eba7c978c3d3fff2ff25ce4efec1c775cdae2ad
xaviguasch <xaviguasch@gmail.com>
2021-02-04 11:46:31 +0100

    added 001 join two strings exercise

---
+++ 001-join-two-strings.js
@@ -0,0 +1,13 @@
+// Join Two Strings
+// You are given two strings as arguments. Join the strings and return the result.
+
+// Requirements
+// Must return a single string
+
+// Example #1
+// solve("ab", "ba")
+// > "abba"
+
+// Example #2
+// solve("race", "car")
+// > "racecar"
