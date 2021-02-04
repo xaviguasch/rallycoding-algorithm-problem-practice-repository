8d83485755aa742ca5505462adc5eb1bc613b615
xaviguasch <xaviguasch@gmail.com>
2021-02-04 11:55:59 +0100

    added 002 add two numbers challenge

---
+++ 002-add-two-numbers.js
@@ -0,0 +1,20 @@
+// Add Two Numbers
+// You are given two integers as arguments. Add the arguments together and return the sum.
+
+// Requirements
+// Must return a single integer
+// Must work with negative integers
+
+// Example #1
+// solve(10, 10)
+// > 20
+
+// Example #2
+// solve(-90, -90)
+// > -180
+
+const solve = (intOne, intTwo) => {
+  return
+}
+
+console.log(solve(10, 10))
